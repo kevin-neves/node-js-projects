0b0e8bf4891f654a459d223c0dc87f0d19190869
kevin-neves <kevinjrneves@gmail.com>
2022-05-17 11:06:50 -0300

    added build production script

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
@@ -7,7 +8,13 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
 app.use(express.json());
-app.use('/', planetsRouter)
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, ',,', 'public', 'index.html'))
+})
 
 module.exports = app;
